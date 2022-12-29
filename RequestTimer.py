d5df57c2fffb638cea8f8a1073c56cf379180768
JWest11 <114183436+JWest11@users.noreply.github.com>
2022-12-28 17:40:05 -0700

    updated

--- RequestTimer.py
+++ RequestTimer.py
@@ -1,5 +1,4 @@
-import json
-import sched, datetime, urllib3, time
+import sched, datetime, urllib3, time, json
 http = urllib3.PoolManager()
 
 inputTimeStrings = ["09:15:25","11:58:23", "00:01:09", "16:01:00", "16:38:00", "12983:djkahfls:jklahkfdjg"]
@@ -21,8 +20,22 @@ def show_queue(schedulerObject):
 
     
 request = http.request("GET", "ifconfig.co/json")
-print(request.data)
+requestDataJsonString = json.loads(request.data.decode("utf-8"))
 
+file = open("Output.txt", "r")
+
+try:
+    data = json.loads(file.read())
+    data.append(requestDataJsonString)
+    data = json.dumps(data)
+except:
+    data = json.dumps([requestDataJsonString])
+file.close()
+
+
+file = open("Output.txt", "w")
+file.write(data)
+file.close()
 
 scheduler = sched.scheduler(time.time, time.sleep)
 
