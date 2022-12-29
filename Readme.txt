2976b810445a377c0176dde89bc1a91c9863f032
JWest11 <114183436+JWest11@users.noreply.github.com>
2022-12-29 16:16:42 -0700

    completed

---
+++ Readme.txt
@@ -0,0 +1,5 @@
+Run the application RequestTimer.py and follow the promts.
+The application asks for the times formatted as follows HH:MM:SS separated by commas.
+It can be pasted in or a file containing them can be selected.
+The Input times will be put into the scheduler to be performed on the current day at the times. Times in the past will be ignored, and invalid inputs will be ignored.
+The get requests will be performed at the times specified, and the output data is output to output.txt as json.
--- RequestTimer.py
+++ RequestTimer.py
@@ -1,60 +1,78 @@
-import sched, datetime, urllib3, time, json
+import sched, datetime, urllib3, time, json, os
 http = urllib3.PoolManager()
 
-inputTimeStrings = ["09:15:25","11:58:23", "00:01:09", "16:01:00", "16:38:00", "12983:djkahfls:jklahkfdjg"]
 
 def get_request(schedulerObject):
     
     request = http.request("GET", "ifconfig.co/json")
+
+    requestDataJsonString = json.loads(request.data.decode("utf-8"))
+
+    file = open("Output.txt", "a")
+    file.close()
+    file = open("Output.txt", "r")
+
+    try:
+        data = json.loads(file.read())
+        data.append(requestDataJsonString)
+        data = json.dumps(data)
+    except:
+        data = json.dumps([requestDataJsonString])
+    file.close()
+
+    file = open("Output.txt", "w")
+    file.write(data)
+    file.close()
+
     print(f"Get request performed at {datetime.datetime.now().isoformat()}")
-    print(request.data)
+    print(requestDataJsonString)
     if not schedulerObject.empty():
         show_queue(schedulerObject)
     else:
         print("Scheduler has no more tasks.")
+
 
 def show_queue(schedulerObject):
     print("CurrentQueue:")
     for event in schedulerObject.queue:
         print(datetime.datetime.fromtimestamp(event.time).isoformat())
 
-    
-request = http.request("GET", "ifconfig.co/json")
-requestDataJsonString = json.loads(request.data.decode("utf-8"))
 
-file = open("Output.txt", "r")
+def app():
+    print("")
+    inputString = input('Plese enter times in quotation marks separated by commas.\nExample: 09:15:25,11:58:23,00:01:09\nTimes in the past will not be run, and invalid inputs are ignored (ie 06:00:00 will not be run if the current time is 07:00:00)\nalternatively type "file" to select a file with the times formatted in the same way\n')
+    if inputString == "file":
+        fileName = input("Input the file name including extension. Example: Input.txt\n")
+        try:
+            file = open(fileName, "r")
+            inputTimeList = file.read().split(',')
+        except:
+            print("file not found")
+            app()
+    else:
+        inputTimeList = inputString.split(',')
 
-try:
-    data = json.loads(file.read())
-    data.append(requestDataJsonString)
-    data = json.dumps(data)
-except:
-    data = json.dumps([requestDataJsonString])
-file.close()
+    scheduler = sched.scheduler(time.time, time.sleep)
 
+    for timeString in inputTimeList:
 
-file = open("Output.txt", "w")
-file.write(data)
-file.close()
+        try:
+            timeComponent = datetime.time.fromisoformat(timeString)
+        except:
+            continue
 
-scheduler = sched.scheduler(time.time, time.sleep)
+        dt = datetime.datetime.combine(datetime.date.today(), timeComponent)
+        if dt.timestamp() < time.time():
+            continue
+        scheduler.enterabs(time=dt.timestamp(), action=get_request, priority=0, argument=[scheduler])
 
-for timeString in inputTimeStrings:
+    if not scheduler.empty():
+        print("Scheduler is running, get requests will be performed at the times specified. Keep this window open.")
+        show_queue(scheduler)
+        scheduler.run()
+    else:
+        print("No valid tasks, check input values.")
+        app()
 
-    try:
-        timeComponent = datetime.time.fromisoformat(timeString)
-    except:
-        continue
-
-    dt = datetime.datetime.combine(datetime.date.today(), timeComponent)
-    if dt.timestamp() < time.time():
-        continue
-    scheduler.enterabs(time=dt.timestamp(), action=get_request, priority=0, argument=[scheduler])
-
-if not scheduler.empty():
-    print("Scheduler is running, get requests will be performed at the times specified. Keep this window open.")
-    show_queue(scheduler)
-    scheduler.run()
-else:
-    print("No valid tasks, check input values.")
-
+os.system("cls" if os.name == "nt" else "clear")
+app()
